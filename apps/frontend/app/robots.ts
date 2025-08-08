ad24e82328f3c395fabd598847b62518cb2ad4df
pavelbeard <pavelborodin1995@gmail.com>
2025-08-08 12:07:06 +0200

    fix: Update sitemap URLs for consistency across the application

--- apps/frontend/app/robots.ts
+++ apps/frontend/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/api/"],
       },
     ],
-    sitemap: "https://conjugador-catalan.vercel.app/sitemap.xml",
+    sitemap: "https://catala-conjugador.vercel.app/sitemap.xml",
   };
 }
--- apps/frontend/app/sitemap.ts
+++ apps/frontend/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://conjugador-catalan.vercel.app/verbs",
+      url: "https://catalan-conjugador.vercel.app/verbs",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1.0,
     },
     {
-      url: "https://conjugador-catalan.vercel.app/settings",
+      url: "https://catalan-conjugador.vercel.app/settings",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: "https://conjugador-catalan.vercel.app/translator",
+      url: "https://catalan-conjugador.vercel.app/translator",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.8,
